3b52a35166fbac3dd1e3a2300d717e1536193e40
Andrew Fillenwarth <andyf016@gmail.com>
2021-01-20 22:50:00 -0500

    set up batch api call

--- quant_value.py
+++ quant_value.py
@@ -9,5 +9,23 @@ from secrets import IEX_CLOUD_API_TOKEN
 stocks = pd.read_csv('sp_500_stocks.csv')
 symbol = 'aapl'
 api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
-data = requests.get(api_url)
+data = requests.get(api_url).json()
 
+price = data['latestPrice']
+pe_ratio = data['peRatio']
+
+print(price)
+print(pe_ratio)
+
+def chunks(lst, n):
+    """ Yield successive n-sized chunks from lst """
+    for i in range(0, len(lst), n):
+        yield lst[i:i+n]
+
+symbol_groups = list(chunks(stocks['Ticker'], 100))
+symbol_strings = []
+for i in range(0, len(symbol_groups)):
+    symbol_strings.append(','.join(symbol_groups[i]))
+#   print(symbol_strings[i])
+
+my_columns = ['Ticker', 'Price', 'Price-to-Earnings Ratio', 'Number of Shares to Buy']
