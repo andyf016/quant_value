682661eb0a8c7e44f15ad4f93821cc8b82e202b8
Andrew Fillenwarth <andyf016@gmail.com>
2021-01-20 22:37:56 -0500

    making successful api calls

--- quant_value.py
+++ quant_value.py
@@ -4,6 +4,10 @@ import xlsxwriter
 import requests
 from scipy import stats
 import math
+from secrets import IEX_CLOUD_API_TOKEN
 
 stocks = pd.read_csv('sp_500_stocks.csv')
-print(stocks)
+symbol = 'aapl'
+api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
+data = requests.get(api_url)
+
